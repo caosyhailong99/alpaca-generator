aab4b8f94ea66e3fd43dd6090a23623ce7b72390
longcsh99 <hailong@atlantic.edu.vn>
2023-08-21 21:44:38 +0700

    Create UI for alpaca generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'alpaca-generator';
+  accessoriesOption = [
+    'Hair',
+    'Ears',
+    'Eyes',
+    'Mouth',
+    'Neck',
+    'Leg',
+    'Accessories',
+    'Background',
+  ];
+  stylesOption = ['Default', 'Curls', 'Short', 'Bang', 'Elegant', 'Quiff'];
 }
