c1a8ea55cb39dcb67b936a884f42a750b32628ce
longcsh99 <hailong@atlantic.edu.vn>
2023-08-21 22:30:46 +0700

    Implement buttons to change alpaca picture's appearance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alpaca-generator';
+  currentPart = 'Hair';
+  chosenHair = 'Default';
+  chosenEars = 'Default';
+  chosenEyes = 'Default';
+  chosenMouth = 'Default';
+  chosenNeck = 'Default';
+  chosenLeg = 'Default';
+  chosenAccessory = 'Earings';
+  chosenBackground = 'blue50';
   accessoriesOption = [
     'Hair',
     'Ears',
@@ -17,5 +26,83 @@ export class AppComponent {
     'Accessories',
     'Background',
   ];
-  stylesOption = ['Default', 'Curls', 'Short', 'Bang', 'Elegant', 'Quiff'];
+  hairStyleOption = ['Default', 'Curls', 'Short', 'Bang', 'Elegant', 'Quiff'];
+  earsStyleOption = [
+    { name: 'Default', value: 'default' },
+    { name: 'Tilt backward', value: 'tilt-backward' },
+    { name: 'Tilt forward', value: 'tilt-forward' },
+  ];
+  eyesStyleOption = ['Default', 'Angry', 'Naughty', 'Panda', 'Smart', 'Star'];
+  mouthStyleOption = ['Default', 'Astonished', 'Eating', 'Laugh', 'Tongue'];
+  neckStyleOption = [
+    { name: 'Default', value: 'default' },
+    { name: 'Bend backward', value: 'bend-backward' },
+    { name: 'Bend forward', value: 'bend-forward' },
+    { name: 'Thick', value: 'thick' },
+  ];
+  legStyleOption = [
+    { name: 'Default', value: 'default' },
+    { name: 'Bubble tea', value: 'bubble-tea' },
+    { name: 'Cookie', value: 'cookie' },
+    { name: 'Game console', value: 'game-console' },
+    { name: 'Tilt backward', value: 'tilt-backward' },
+    { name: 'Tilt forward', value: 'tilt-forward' },
+  ];
+  accessoriesStyleOption = ['Earings', 'Flower', 'Glasses', 'Headphone'];
+  backgroundStyleOption = [
+    'blue50',
+    'blue60',
+    'blue70',
+    'darkblue30',
+    'darkblue50',
+    'darkblue70',
+    'green50',
+    'green60',
+    'green70',
+    'grey40',
+    'grey70',
+    'grey80',
+    'red50',
+    'red60',
+    'red70',
+    'yellow50',
+    'yellow60',
+    'yellow70',
+  ];
+
+  onClickPart(part: string) {
+    this.currentPart = part;
+  }
+
+  onClickHairStyle(hairStyle: string) {
+    this.chosenHair = hairStyle;
+  }
+
+  onClickEarsStyle(earsStyle: string) {
+    this.chosenEars = earsStyle;
+  }
+
+  onClickEyesStyle(eyesStyle: string) {
+    this.chosenEyes = eyesStyle;
+  }
+
+  onClickMouthStyle(mouthStyle: string) {
+    this.chosenMouth = mouthStyle;
+  }
+
+  onClickNeckStyle(neckStyle: string) {
+    this.chosenNeck = neckStyle;
+  }
+
+  onClickLegStyle(legStyle: string) {
+    this.chosenLeg = legStyle;
+  }
+
+  onClickAccessoriesStyle(accessoriesStyle: string) {
+    this.chosenAccessory = accessoriesStyle;
+  }
+
+  onClickBackgroundStyle(backgroundStyle: string) {
+    this.chosenBackground = backgroundStyle;
+  }
 }
